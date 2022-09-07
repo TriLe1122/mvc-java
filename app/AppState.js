27f1d346d4c6ad5704138317b25a6a353ab6064b
Trl Le <letrin93@gmail.com>
2022-09-06 20:46:58 -0600

    commit

--- app/AppState.js
+++ app/AppState.js
@@ -11,7 +11,7 @@ class AppState extends EventEmitter {
 
   // NOTE this type line just adds funtionality to vs code, telling it only Players can exist in that array
   /** @type {import('./Models/Snack').Snack[]} */
-  snacks = [new Snack('Snack1', .25, 0), new Snack('Snack2', .25, 0), new Snack('Snack3', .25, 0)]
+  snacks = [new Snack('Snack1', 5.5, 0), new Snack('Snack2', 3.25, 0), new Snack('Snack3', 1.25, 0), new Snack('Snack4', 5.25, 0)]
 
 
 
--- app/Controllers/SnackController.js
+++ app/Controllers/SnackController.js
@@ -1,10 +1,24 @@
 import { appState } from "../AppState.js";
 import { snackService } from "../Services/SnackService.js";
 
+// NOTE when the user pushes a button or something that makes the page change or something
+
+
+function _drawTotal() {
+  let total = 0
+  let totalDOM = document.getElementById('cartTotal')
+  let snacks = appState.snacks
+  snacks.forEach(snack => {
+    total += snack.price * snack.quantity
+  })
+  totalDOM.innerText = total
+}
 
 
 
 // NOTE functions on the outside of the class are PRIVATE, meaning the user cannot access them
+// NOTE VVV this is grabbing the information from appstate.js and then turning it into a template from our snack.js
+// for  each snack we have, its nice, price, and amount
 function _drawSnacks() {
   let snacks = appState.snacks
   let template = ''
@@ -13,37 +27,37 @@ function _drawSnacks() {
 }
 
 
+// NOTE this is grabbing the template from the snack.js and adding to the cart everytime we hit the add item to the cart
+function _drawCart() {
+  let cartSnacks = appState.snacks;
+  let template = '';
+  cartSnacks.forEach((p) => {
+
+    template += p.cartTemplate
+
+  });
+  // cartSnacks.forEach(s => template += s.cartTemplate)
+  document.getElementById('cart').innerHTML = template;
+}
+
+
+// NOTE this is what starts up and runs automatically. 
+// its using the drawsnacks function ^^^^ line 20
 export class SnackController {
   // place calls you want to run on load in the controller constructor
   constructor() {
-    // NOTE for demonstration purposed
-    // new Player('Mick', 0)
-    // new Player('Jeffe', -10)
-    // this.drawPlayers()
     _drawSnacks()
   }
 
 
   buySnack(name) {
-    console.log( 'Buying Snack:', name,);
+    console.log('Buying Snack:', name,);
     snackService.buySnack(name)
     _drawSnacks()
+    _drawCart()
+    _drawTotal()
     // this.drawPlayers()
   }
 
-  drawSnacks() {
-    let snacks = appState.snacks
-    let template = ''
-    snacks.forEach(p => {
-      template += `
-      <div class="col-md-6 text-center p-2 bg-light elevation-2">
-        <h3>${p.name}</h3>
-        <h3 class="text-warning">Price: ${p.price}</h3>
-        <h3 class="text-warning">Quantity: ${p.quantity}</h3>
-        <button class="btn btn-primary w-100" onclick="app.snackController.buySnack('${p.name}')">+</button>
-      </div>
-      `
-    })
-    document.getElementById('snacks').innerHTML = template
-  }
+
 }
